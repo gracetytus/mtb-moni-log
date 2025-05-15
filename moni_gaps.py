dac588ac356b1c0beb89e55544916fbeeac7c66d
Grace <gtytus@hawaii.edu>
2025-05-15 10:08:28 -1000

    add all fields from MTBMoniData to be read out for each crash, will probably be inescapably slow

--- moni_gaps.py
+++ moni_gaps.py
@@ -29,8 +29,15 @@ if __name__ == '__main__':
                     moni = go.tof.monitoring.MtbMoniData()
                     moni.from_tofpacket(tp)
                     tiu_busy = moni.tiu_busy_len
+                    daq_queue = moni.daq_queue_len
+                    temp = moni.temp
+                    rate = moni.rate
+                    lost_rate = moni.lost_rate
+                    vccint = moni.vccint
+                    vccbram = moni.vccbram
+                    vccaux = moni.vccaux
 
-                    packet_ts.append((gcu, tiu_busy))
+                    packet_ts.append((gcu, tiu_busy, daq_queue, temp, lost_rate, vccint, vccbram, vccaux))
 
 
     packet_ts.sort()
@@ -40,16 +47,31 @@ if __name__ == '__main__':
         duration = end - start
 
         tiu = packet_ts[i-1][1]
+        daq = packet_ts[i-1][2]
+        t = packet_ts[i-1][3]
+        r = packet_ts[i-1][4]
+        lr = packet_ts[i-1][5]
+        vint = packet_ts[i-1][6]
+        vbram = packet_ts[i-1][7]
+        vaux = packet_ts[i-1][8]
 
         if duration >= args.window:
-            moni_gaps.append((start, end, duration, tiu))
+            moni_gaps.append((start, end, duration, tiu, daq, t, r, lr, vint, vbram, vaux))
 
 
     print('--------------------------------------------------------------------------------------------')
     print('Detected ' + str(len(moni_gaps))+ ' MTB outages with lenth greater than '+ str(args.window) + ' seconds between ' + str(args.start_time) + ' to '+ str(args.end_time) + ' seconds')
     for gap in moni_gaps:
         print(f'from {gap[0]} to {gap[1]} with duration {gap[2]}')
-        print(f'the tiu_busy_count before the crash was {gap[3]}')
+        print(f'---the tiu_busy_count before the crash was {gap[3]}')
+        print(f'---the daq queue length before the crash was {gap[4]}')
+        print(f'---the temperature before the crash was {gap[5]}')
+        print(f'---the rate before the crash was {gap[6]}')
+        print(f'---the lost rate before the crash was {gap[7]}')
+        print(f'---the vccint before the crash was {gap[8]}')
+        print(f'---the vccbram before the crash was {gap[9]}')
+        print(f'---the vaux before the crash was {gap[10]}')
+        print('------------------------------------------------')
 
     print('--------------------------------------------------------------------------------------------')
 
